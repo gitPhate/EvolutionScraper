1c30d9cbf0d1f65fc20eab53fee1a9148d8e7889
gitPhate <gabriele.ricci.33.95@gmail.com>
2025-09-29 19:03:03 +0200

    booking launcher system

--- EvolutionScraper/Program.cs
+++ EvolutionScraper/Program.cs
@@ -1,11 +1,39 @@
 ﻿
 using EvolutionScraper;
-using System.Text.Json;
 
-EvolutionScraperOptions opt =
-    JsonSerializer.Deserialize<EvolutionScraperOptions>(File.ReadAllText(@"appsettings.json"))
-    ?? throw new JsonException("Unable to deserialize options");
+Console.WriteLine("Evolution scraper v1.0");
 
-EvolutionScraper.EvolutionScraper scraper = new(opt);
-bool isBooked = await scraper.BookClassAsync("Pilates", DayOfWeek.Monday, new TimeOnly(8, 30));
-Console.ReadLine();
+Console.WriteLine("Reading configs");
+
+Settings settings = Settings.NewFromFile();
+bool somethingFound = false;
+
+foreach (var kvp in settings.Bookings)
+{
+    DayOfWeek triggerDay = (DayOfWeek)((int)(kvp.Key - 3 + 7) % 7);
+    if (DateTime.Now.DayOfWeek != triggerDay)
+    {
+        continue;
+    }
+
+    somethingFound = true;
+    Console.WriteLine($"Time to book: {kvp.Value.Name} scheduled for {kvp.Key} at {kvp.Value.Time}");
+    using EvolutionScraper.EvolutionScraper scraper = new(settings.EvolutionScraperOptions);
+
+    Console.WriteLine("Booking #1 class");
+    bool isBooked = await scraper.BookClassAsync(kvp.Value.Name, kvp.Key, kvp.Value.Time);
+    if (isBooked)
+    {
+        Console.WriteLine("Booked successfully");
+    }
+    else
+    {
+        Console.WriteLine("Something went wrong with booking");
+    }
+}
+
+if (!somethingFound)
+{
+    Console.WriteLine("Nothing to book found");
+}
+
---
+++ EvolutionScraper/Settings.cs
@@ -0,0 +1,49 @@
+﻿#nullable disable
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EvolutionScraper
+{
+    internal class Settings
+    {
+        private const string SettingsFilePath = "appsettings.json";
+
+        private static readonly JsonSerializerOptions _jsonSerializerOptions = GetJsonSerializerOptions();
+
+        [JsonRequired]
+        public EvolutionScraperOptions EvolutionScraperOptions { get; set; }
+
+        [JsonRequired]
+        public Dictionary<DayOfWeek, ClassBooking> Bookings { get; set; }
+
+        public static Settings NewFromFile(string filePath = null)
+        {
+            string settingsFilePath = filePath ?? SettingsFilePath;
+
+            if (!File.Exists(settingsFilePath))
+            {
+                throw new FileNotFoundException($"Missing config file {settingsFilePath}");
+            }
+
+            string settingsFileContent = File.ReadAllText(settingsFilePath);
+            Settings settings = JsonSerializer.Deserialize<Settings>(settingsFileContent, _jsonSerializerOptions)
+                ?? throw new InvalidOperationException($"Unable to deserialize config file {settingsFilePath}");
+
+            return settings;
+        }
+
+        private static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            JsonSerializerOptions jsonSerializerOptions = new();
+            jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter<DayOfWeek>());
+            return jsonSerializerOptions;
+        }
+    }
+
+    public class ClassBooking
+    {
+        public string Name { get; set; }
+        public TimeOnly Time { get; set; }
+    }
+}
